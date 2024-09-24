ae2b4213b90a1d31f16ad7959341b06ea061bb38
unknown <61843707+Akash-ik@users.noreply.github.com>
2024-09-24 20:21:59 +0530

    init

---
+++ src/test/java/com/template_1/template_1/Template1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.template_1.template_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Template1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
